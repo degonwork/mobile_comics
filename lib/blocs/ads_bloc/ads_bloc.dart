9988296485154a769af6a30f5220283c69bba12f
Thanh Long <dtlong.work@gmail.com>
2023-06-21 15:49:12 +0700

    abc

--- lib/blocs/ads_bloc/ads_bloc.dart
+++ lib/blocs/ads_bloc/ads_bloc.dart
@@ -1,38 +1,36 @@
-
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:full_comics_frontend/config/app_constant.dart';
 import 'package:full_comics_frontend/data/repository/ads_repository.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-
 part 'ads_event.dart';
 part 'ads_state.dart';
 
 class AdsBloc extends Bloc<AdsEvent, AdsState> {
-  
   AdsBloc() : super(ADsShow(0)) {
     on<Increment>(_increment);
     on<Reset>(_reset);
   }
-  Future<void> _increment(Increment event,Emitter<AdsState> emitter)async{
-    final SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+  Future<void> _increment(Increment event, Emitter<AdsState> emitter) async {
+    final SharedPreferences sharedPreferences =
+        await SharedPreferences.getInstance();
     int adsTimesIn = 1;
     final adsTimes = sharedPreferences.getInt(AppConstant.timeAdsLocal);
-      if (adsTimes != null && adsTimes > 0) {
-        adsTimesIn = adsTimes;
+    if (adsTimes != null && adsTimes > 0) {
+      adsTimesIn = adsTimes;
+    }
+    if (state is ADsShow) {
+      // print('${(state as ADsShow).adsTimes} bnxjhnxhzxhxxdhsdhdsd');
+      if ((state as ADsShow).adsTimes >= adsTimesIn) {
+        ADSRepo.showADS();
+        add(Reset());
+      } else {
+        (state as ADsShow).adsTimes += 1;
       }
-      if (state is ADsShow) {
-        // print('${(state as ADsShow).adsTimes} bnxjhnxhzxhxxdhsdhdsd');
-        if ((state as ADsShow).adsTimes >= adsTimesIn) {
-          ADSRepo.showADS();
-          add(Reset());
-        }else{
-          (state as ADsShow).adsTimes += 1;
-        }
-      }
-  
+    }
   }
-   void _reset(Reset event, Emitter<AdsState> emitter){
+
+  void _reset(Reset event, Emitter<AdsState> emitter) {
     if (state is ADsShow) {
       (state as ADsShow).adsTimes = 0;
     }
